a72cf99a86447725316b052a140826bca88d035c
Gabe Krahulik <gabekrahulik@gmail.com>
2026-05-26 15:38:33 -0700

    better magic missile targeting

--- src/Spells/Fire/Firebolt.ts
+++ src/Spells/Fire/Firebolt.ts
@@ -7,14 +7,13 @@ createProjectileSpell({
   spawn: (tag) => spawnSingleBolt(tag, 50),
   projectile: {
     lifetime: 50,
+    destroyOnHit: true,
+    blockCollision: true,
     move: () => tp('@s', loc(0, 0, 1.2)),
     particles: () => particle('flame', rel(0, 0, 0), abs(0.1, 0.1, 0.1), 0.01, 5, 'force'),
-    onHit: () => { damage('@s', 2, 'on_fire'); Burning.apply(Variable(2)); },
-    onExpire: () => {
-      execute.as(Selector('@e', { distance: [0, 4], type: '#arcane_arts:targetable' })).run(() => {
-        damage('@s', 3, 'on_fire');
-        Burning.apply(Variable(2));
-      });
-    }
+    onHit: () => { 
+      damage('@s', 2, 'on_fire'); 
+      Burning.apply(Variable(2)); 
+    },
   }
 });
--- src/Spells/Ice/Frostbolt.ts
+++ src/Spells/Ice/Frostbolt.ts
@@ -7,14 +7,13 @@ createProjectileSpell({
   spawn: (tag) => spawnSingleBolt(tag, 50),
   projectile: {
     lifetime: 50,
+    destroyOnHit: true,
+    blockCollision: true,
     move: () => tp('@s', loc(0, 0, 1.2)),
     particles: () => particle('snowflake', rel(0, 0, 0), abs(0.1, 0.1, 0.1), 0.01, 5, 'force'),
-    onHit: () => { damage('@s', 1, 'freeze'); Freezing.apply(Variable(2)); },
-    onExpire: () => {
-      execute.as(Selector('@e', { distance: [0, 4], type: '#arcane_arts:targetable' })).run(() => {
-        damage('@s', 1, 'freeze');
-        Freezing.apply(Variable(2));
-      });
-    }
+    onHit: () => { 
+      damage('@s', 1, 'freeze'); 
+      Freezing.apply(Variable(2)); 
+    },
   }
 });
