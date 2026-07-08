0cc594e37de7abde6ff03a606d69e9424cd8fce1
Gabe Krahulik <gabekrahulik@gmail.com>
2026-07-07 21:01:36 -0700

    :sparkles: Added in stickers for spellcasting and entering booth

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { MCFunction, Selector } from 'sandstone'
+import { advancement, MCFunction, Selector } from 'sandstone'
 import { SelectorProperties } from 'sandstone/variables'
 
 import { ticking } from './shared'
@@ -6,8 +6,8 @@ import './sections/main'
 import './version'
 // TODO: Disabled while developing presentation
 //import './sections/rhythm/index'
-//import './sections/magic/index'
-import './sections/presentation/index'
+import './sections/magic/index'
+// import './sections/presentation/index'
 
 MCFunction('main', () => {
     const all_players = (args: SelectorProperties<false, false, 'minecraft:player'>) => Selector(
@@ -20,6 +20,10 @@ MCFunction('main', () => {
 
 MCFunction('init_player', () => {
     const player = Selector('@s')
+
+     // TODO: uncomment when stickers work
+    // grant the visit sticker advancement
+    advancement.grant(player).only('summit.sticker_book:sandstone_summit_booth/enter_booth')
 })
 
 MCFunction('clean_player', () => {
--- src/sections/main.ts
+++ src/sections/main.ts
@@ -2,10 +2,6 @@ import { MCFunction, Selector } from 'sandstone'
 
 MCFunction('sections/main/init_player', () => {
     const player = Selector('@s')
-
-    // TODO: uncomment when stickers work
-    // grant the visit sticker advancement
-    // advancement.grant(player).only('summit.sticker_book:sandstone_summit_booth/enter_booth')
 })
 
 MCFunction('sections/main/clean_player', () => {
