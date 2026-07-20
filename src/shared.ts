9ae16a7b3d9bfc0c46ec73d48435d12e685be799
MulverineX <mulverin3@gmail.com>
2026-07-19 19:54:39 -0600

    🧹 Add `BOOTH_ENTITY_TAG` to shared.ts

--- src/shared.ts
+++ src/shared.ts
@@ -5,4 +5,6 @@ import config from '../sandstone.config'
 export const NAMESPACE = config.namespace!
 export const PROJECT_ROOT = resolve(import.meta.dirname, '..')
 
+export const BOOTH_ENTITY_TAG = `summit.booth_entity.${NAMESPACE}` as const
+
 export const ticking = Tag('function', 'ticking', [])
