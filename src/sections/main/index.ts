382372856271bd432119341eca61cba90ea1d414
Gabe Krahulik <gabekrahulik@gmail.com>
2026-07-30 10:25:45 -0700

    🐛 Add summon for missing bridge block displays

--- src/sections/main/index.ts
+++ src/sections/main/index.ts
@@ -1,9 +1,17 @@
 import { join } from 'path'
-import { Font, Model, NBT, Texture, Variant } from 'sandstone'
+import { Font, MCFunction, Model, NBT, raw, Texture, Variant, Tag } from 'sandstone'
 
 import monospace from '../../../resources/assets/font/monospace/providers.json'
 
 import './showcase'
+
+
+// Summon for missing bridge block displays
+const summonBridgeDisplays = MCFunction('sections/main/summon_missing_bridge_displays', () => {
+    raw('summon minecraft:block_display -71.0 65.0 65.0 {Tags:["summit.static","summit.booth_entity.sandstone_summit_booth"],Passengers: [{Tags:["summit.static","summit.booth_entity.sandstone_summit_booth"],block_state: {Name: "minecraft:yellow_bed", Properties: {facing: "north", occupied: "false", part: "foot"}}, id: "minecraft:block_display", transformation: {left_rotation: [0.0f, 0.0f, 0.0f, 1.0f], right_rotation: [0.0f, 0.0f, 0.0f, 1.0f], scale: [1.0f, 1.0f, 1.0f], translation: [1.0f, 0.0f, 0.0f]}}, {Tags:["summit.static","summit.booth_entity.sandstone_summit_booth"],block_state: {Name: "minecraft:yellow_bed", Properties: {facing: "north", occupied: "false", part: "foot"}}, id: "minecraft:block_display", transformation: {left_rotation: [0.0f, 0.0f, 0.0f, 1.0f], right_rotation: [0.0f, 0.0f, 0.0f, 1.0f], scale: [1.0f, 1.0f, 1.0f], translation: [2.0f, 0.0f, 0.0f]}}, {Tags:["summit.static","summit.booth_entity.sandstone_summit_booth"],block_state: {Name: "minecraft:yellow_bed", Properties: {facing: "north", occupied: "false", part: "foot"}}, id: "minecraft:block_display", transformation: {left_rotation: [0.0f, 0.0f, 0.0f, 1.0f], right_rotation: [0.0f, 0.0f, 0.0f, 1.0f], scale: [1.0f, 1.0f, 1.0f], translation: [0.0f, -1.0f, 2.0f]}}, {Tags:["summit.static","summit.booth_entity.sandstone_summit_booth"],block_state: {Name: "minecraft:yellow_bed", Properties: {facing: "north", occupied: "false", part: "foot"}}, id: "minecraft:block_display", transformation: {left_rotation: [0.0f, 0.0f, 0.0f, 1.0f], right_rotation: [0.0f, 0.0f, 0.0f, 1.0f], scale: [1.0f, 1.0f, 1.0f], translation: [2.0f, -1.0f, 2.0f]}}, {Tags:["summit.static","summit.booth_entity.sandstone_summit_booth"],block_state: {Name: "minecraft:yellow_bed", Properties: {facing: "north", occupied: "false", part: "foot"}}, id: "minecraft:block_display", transformation: {left_rotation: [0.0f, 0.0f, 0.0f, 1.0f], right_rotation: [0.0f, 0.0f, 0.0f, 1.0f], scale: [1.0f, 1.0f, 1.0f], translation: [1.0f, -1.0f, 2.0f]}}], block_state: {Name: "minecraft:yellow_bed", Properties: {facing: "north", occupied: "false", part: "foot"}}}')
+})
+Tag('function', 'summit.booth:sandstone_summit_booth/entities/summon', [summonBridgeDisplays], { onConflict: 'append' })
+
 
 const asset = (...path: string[]) => Bun.file(
     join(process.cwd(), 'resources', 'assets', ...path)
