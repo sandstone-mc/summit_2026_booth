1ab0815d2c0a7e2c188fc13cb1f2f12dfa36c4f8
MulverineX <mulverin3@gmail.com>
2026-08-01 20:39:27 -0600

    🖋️ Fix credit role/description for Symarol

--- src/sections/npcs/npcs/credits.ts
+++ src/sections/npcs/npcs/credits.ts
@@ -66,7 +66,7 @@ export const CREDITS: CreditSection[] = [
     {
         section: 'Music',
         members: [
-            { name: 'Symarol', minecraft: 'bd8a4b30-2f86-4da0-b444-2c7a9b096d23', socials: [{ type: 'instagram', url: 'https://www.instagram.com/symarol/' }, { type: 'bluesky', url: 'https://bsky.app/profile/symarol.bsky.social' }], role: 'NBS Transcriptions of Porter Robinson & Jamie Paige tracks' },
+            { name: 'Symarol', minecraft: 'bd8a4b30-2f86-4da0-b444-2c7a9b096d23', socials: [{ type: 'instagram', url: 'https://www.instagram.com/symarol/' }, { type: 'bluesky', url: 'https://bsky.app/profile/symarol.bsky.social' }], role: 'Noteblock arrangements of the Porter Robinson tracks' },
             { name: 'Porter Robinson', socials: [{ type: 'instagram', url: 'https://www.instagram.com/porterrobinson' }, { type: 'bluesky', url: 'https://bsky.app/profile/porterrobinson.com' }] },
             { name: 'Jamie Paige', socials: [{ type: 'website', url: 'https://jamies.page/' }, { type: 'bluesky', url: 'https://bsky.app/profile/jamies.page' }] },
         ],
